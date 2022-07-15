0b48fb7448aeda1501f6d0461abbbab89a732df1
Ivan Garcia <ivanggv2@gmail.com>
2022-07-15 18:03:44 +0200

    initial master

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
